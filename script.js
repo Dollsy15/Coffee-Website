88c3487f48408aa4375d8a33f1e906d3773bd5b9
Dollsy Rani <dollsyrani155@gmail.com>
2025-04-13 12:14:20 +0530

    some swiper js

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const menuOpenButton = document.getElementById('menu-open-button');
-const menuCloseButton = document.getElementById('menu-close-button');
+const menuOpenButton = document.getElementById("menu-open-button");
+const menuCloseButton = document.getElementById("menu-close-button");
 const body = document.body;
 
-menuOpenButton.addEventListener('click', () => {
-    body.classList.add('show-mobile-menu');
+menuOpenButton.addEventListener("click", () => {
+  body.classList.add("show-mobile-menu");
 });
 
-menuCloseButton.addEventListener('click', () => {
-    body.classList.remove('show-mobile-menu');
+menuCloseButton.addEventListener("click", () => {
+  body.classList.remove("show-mobile-menu");
+});
+
+const swiper = new Swiper(".slider-wrapper", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
 });
